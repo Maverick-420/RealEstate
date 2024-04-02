9ddd446873b604988d450f7091ad12f1da293e2f
AniketXcodes <aniketcodes2003@gmail.com>
2024-04-02 23:48:57 +0530

    header component ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
