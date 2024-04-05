1741967d29f6dc0271df44618d260c95fc24dcee
AniketXcodes <aniketcodes2003@gmail.com>
2024-04-05 22:32:25 +0530

    connecting to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("Error in Connecting to DB");
+  });
 const app = express();
 
 app.listen(3000, () => {
